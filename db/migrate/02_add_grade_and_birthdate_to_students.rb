81f7ded3dda66264ffe2dea0492efd523bd74ad5
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-17 17:24:32 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,6 +2,5 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
     add_column :artists, :grade, :string
     add_column :artists, :birthdate, :string
-    end
   end
 end
