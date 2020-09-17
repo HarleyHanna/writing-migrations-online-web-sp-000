11fdf6a3db790817feec986cff1005f0fd1db838
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-17 17:24:12 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeForBirthdate < ActiveRecord::Migration[5.2]
   def change
-    change_column(:artists, :birthdate, :datatype)
+    change_column :artists, :birthdate, :datatype
   end
 end
